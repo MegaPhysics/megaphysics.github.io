99424831b1f41495cb9447337c8ec1575c15fc54
Harry Maclean <harryjmaclean@googlemail.com>
2014-08-29 08:58:18 +0100

    Add a new test which compares rendering of a markdown article to a reference html file. Two new megphys commands: 'test' to run tests, 'update_tests' to recreate reference html file.

--- test.py
+++ test.py
@@ -1,6 +1,45 @@
+import subprocess
 from megaphysics.build import run
+from BeautifulSoup import BeautifulSoup
+
+article_text = """
+---
+title: Test
+course: Articles
+author: Test Author
+---
+Test Article
+====
+
+This article is a test to ensure that the build system works correctly.
+
+"""
 
 
 def test_build():
     run()
     return 0
+
+
+def test_article():
+    with open('articles/test.md', 'w') as f:
+        f.write(article_text)
+    run()
+    test = BeautifulSoup(open('build/Articles/test.html', 'r').read()).prettify()
+    ref = BeautifulSoup(open('test_article_reference.html', 'r').read()).prettify()
+    subprocess.call(["rm", "articles/test.md"])
+    print test
+    print ref
+    assert test == ref, "Rendered article is not the same as reference."
+    return 0
+
+if __name__ == "__main__":
+    """Assume we are being called by megphys to update the tests"""
+    with open('articles/test.md', 'w') as f:
+        f.write(article_text)
+    run()
+    subprocess.call([
+        "cp",
+        "build/Articles/test.html",
+        "test_article_reference.html"])
+    subprocess.call(["rm", "articles/test.md"])
